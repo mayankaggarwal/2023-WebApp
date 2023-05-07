541bc64ad3f14012736ac34f0b50c392dde89f5a
Mayank Aggarwal <mayankaggarwal1989@gmail.com>
2023-05-07 18:21:29 +0530

    Updating root path

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    apiKey: '{SECRET_API_KEY}',
-    apiUrl: '{API_URL}',
-    domain: 'dev-mayank.us.auth0.com',
-    clientId: '{CLIENT_ID}',
-    audience: '{AUDIENCE}'
-  };
--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    apiKey: '{SECRET_API_KEY}',
-    apiUrl: 'http://localhost:5000/WeatherForecast',
-    domain: 'dev-mayank.us.auth0.com',
-    clientId: '',
-    audience: 'http://localhost:5000'
-  };
